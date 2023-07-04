345c5a11e1d02ca8311b88d97554f4a3fd96c72e
likhithreddy28 <likhithreddy150@gmail.com>
2023-07-04 22:32:16 +0530

    coins

---
+++ src/components/Coins.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@mui/material'
+import React from 'react'
+
+const useStyles = makeStyles (()=>({
+       coins : {
+
+       }
+}))
+
+const Coins = () => {
+    const classes = makeStyles() ;
+  return (
+    <div>Coins</div>
+  )
+}
+
+export default Coins
